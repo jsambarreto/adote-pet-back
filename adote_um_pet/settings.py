1bca2105f99b5e4972c3b1c667c0f53c064d2008
Jorge Barreto <jsambarreto@gmail.com>
2022-06-03 15:20:57 -0300

    Fix Bug

--- adote_um_pet/settings.py
+++ adote_um_pet/settings.py
@@ -12,6 +12,7 @@ https://docs.djangoproject.com/en/4.0/ref/settings/
 
 from pathlib import Path
 from pickle import FALSE
+from secret import SECRET_KEY_
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -21,7 +22,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-4c*9mh2!@uxt*tw6u5u!1vk-ra=ydcrg(d^7zf9mv$09$+pmes'
+SECRET_KEY = SECRET_KEY_
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
